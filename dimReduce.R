67f56086a46879fc9e0a22c5ebce2d3752811d46
eflynn90 <eflynn90@gmail.com>
2016-11-14 23:26:58 -0500

    11-14 night

--- dimReduce.R
+++ dimReduce.R
@@ -1,5 +1,10 @@
-etwd and load files
-setwd("/results/G4017_Users/edf2126/team_awesome/")
+#!/usr/bin/Rscript
+
+
+
+## setwd and load files
+print("Loading files...")
+setwd("/home/local/users/eflynn/deepseq/")
 fpkm<-read.csv("datasets/scRNASeq/fpkm_table.csv")
 pheno<-read.csv("datasets/scRNASeq/columns-cells.csv")
 genes<-read.csv("datasets/scRNASeq/rows-genes.csv")
@@ -7,18 +12,21 @@ genes<-read.csv("datasets/scRNASeq/rows-genes.csv")
 
 ## reformat fpkm df
 fpkm[,1] -> row.names(fpkm)                     ## rename rows
-fpkm[,-1] -> fpkm                                       ## remove gene id column
+fpkm[,-1] -> fpkm                               ## remove gene id column
 gsub("^X","",names(fpkm)) -> names(fpkm)        ## remove leading X from cell IDs
-#fpkm <- t(fpkm)                                        ## transform so rows are cells, columns are genes (nvm, do later)
+#fpkm <- t(fpkm)                                ## transform so rows are cells, columns are genes (nvm, do later)
 
 ## Compute PCs of all genes, write to file
 print("Computing principal components on all genes")
-fpkm_pr<-prcomp(t(fpkm),center=T,scale.=F)
+#fpkm_pr<-prcomp(t(fpkm),center=T,scale.=F)
+fpkm_pr<-prcomp(fpkm,center=T,scale.=F)
 unclass(fpkm_pr$rotation) -> fpkm_pr_rot
 fpkm_pr_rot[1:5,1:5]
 write.table(fpkm_pr_rot,file="results/batch_effect/fpkm_pr.txt",quote=FALSE,
         sep="\t", row.names=TRUE, col.names=TRUE)
 
+
+print("Computing principal components on genes fpkm > 0")
 
 ## Isolate genes w/ fpkm > 0
 row_sub = apply(fpkm, 1, function(row) all(row!=0))
@@ -26,9 +34,9 @@ fpkm[row_sub,] -> sub_fpkm
 
 
 ## Compute components and plot
-print("Computing principal components on genes fpkm > 0")
-sub_fpkm_pr<-prcomp(t(sub_fpkm),center=T,scale.=F)
+#sub_fpkm_pr<-prcomp(t(sub_fpkm),center=T,scale.=F)
+sub_fpkm_pr<-prcomp(sub_fpkm,center=T,scale.=F)
 unclass(sub_fpkm_pr$rotation) -> sub_fpkm_pr_rot
-sub_fpkm_rot[1:5,1:5]
+sub_fpkm_pr_rot[1:5,1:5]
 write.table(sub_fpkm_pr_rot,file="results/batch_effect/sub_fpkm_pr.txt",quote=FALSE,
         sep="\t", row.names=TRUE, col.names=TRUE)
